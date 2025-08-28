7b72166c4221716725d4f1439bf49caf85d89053
Mohammadsyedmostafa <syedmostafa884@gmail.com>
2025-08-28 11:28:16 +0600

    added call funtion and call history

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -11,3 +11,59 @@ for (const btn of heartButtons) {
         document.getElementById('heart-count').innerText = counter;
     });
 }
+
+// call button
+const callButtons = document.querySelectorAll('.call-button');
+const coinDisplay = document.getElementById('coin-display');
+const callHistory = document.getElementById('call-history');
+
+for (const btn of callButtons) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        const card = btn.closest('.card');
+        const serviceName = card.querySelector('.service-name').innerText;
+        const serviceNumber = card.querySelector('.service-number').innerText;
+
+        if (coins < 20) {
+            alert("Not enough coins");
+            return;
+        }
+        coins -= 20;
+        coinDisplay.innerText = coins;
+        alert(`Calling ${serviceName} (${serviceNumber})`);
+
+        // Data save
+        const data = {
+            name: serviceName,
+            number: serviceNumber,
+            date: new Date().toLocaleTimeString()
+        };
+        history.push(data);
+
+        // Update Call History
+        callHistory.innerText = "";
+        for (const item of history) {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                    <div class="m-3 flex justify-between items-center">
+                        <div class="">
+                            <h1 class="font-semibold text-md">${item.name}</h1>
+                            <p class="font-normal text-sm">${item.number}</p>
+                        </div>
+                        <div>
+                            <p class="font-normal text-sm">${item.date}</p>
+                        </div>
+                    </div>
+            `;
+            callHistory.appendChild(div);
+        }
+    });
+}
+
+
+// clear button 
+document.getElementById('clear-button').addEventListener('click',function(e){
+    e.preventDefault()
+    callHistory.innerText = ""
+})
+
